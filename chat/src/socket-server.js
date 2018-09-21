516ce2a21120981e38f374012b5d844270f91024
Finskou <lelarhaz@gmail.com>
2018-09-21 15:06:45 +0200

    socker-serve added from previous git

---
+++ chat/src/socket-server.js
@@ -0,0 +1,26 @@
+var express = require('express')
+var socket = require('socket.io')
+
+var app = express()
+
+var server = app.listen(4000, () => {
+  console.log('Time to have fun! Go on port 3000')    
+})
+
+app.use(express.static('./public'))
+
+
+var io = socket(server)
+
+io.on('connection', (socket) => {
+  
+  console.log('We made the socket connection', socket.id)    
+  
+  socket.on('chat', (data) => {
+    io.sockets.emit('chat', data)   
+  }) 
+
+  socket.on('typing', (data) => {
+    socket.broadcast.emit('typing', data)
+  })
+})
