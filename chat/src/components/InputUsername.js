b9cc1933358aaa779a9e0b39638198a4ab39abba
Finskou <lelarhaz@gmail.com>
2018-09-21 15:06:10 +0200

    start InputUsername component

---
+++ chat/src/components/InputUsername.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class InputUsername extends Component {
+    render() {
+      return (
+        <div className="InputUsername">
+          <header className="App-header">
+            
+          </header>
+  
+        </div>
+      );
+    }
+  }
+  
+  export default InputUsername;
+  
