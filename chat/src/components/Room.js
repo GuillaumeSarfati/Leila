1f4b819ac4385159bf423aebd5a568de18329b4f
Finskou <lelarhaz@gmail.com>
2018-09-23 23:24:58 +0200

    TEST trying to make Room a function instead of Component

--- chat/src/components/Room.js
+++ chat/src/components/Room.js
@@ -1,53 +1,79 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-
-export default class Room extends Component {
-	constructor(props){
-		super(props);
-		this.state = {
-		  value: ""
-		};
+const Room = () => {
+	return (
+		<div id="messageArea" className="row">
+						<div className="col-md-3">
+							<div className="well">
+									<h3> Online Users</h3>
+		 							<ul className="list-group" id="users"></ul>
+		 						</div>
+		 					</div>
+		 					<div className="col-md-8 offset-md-8">
+		 						<div className="chat" id="chat"></div>
 		
-		this.handleChange = this.handleChange.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
-	  }
-  
-	  //create methods
-	  handleChange(event){
-		this.setState({value: event.target.value});
-	  }
-  
-	  handleSubmit(event){
-		console.log('Message ' + this.state.value + ' Submitted')
-		//stop the form from refreshing the page on submit
-		event.preventDefault();
-		//this will clear input after submit
-		this.setState({ value: ""});
-	  }
-  
-    render(){
-        return (
-            <div id="messageArea" className="row">
-					<div className="col-md-3">
-						<div className="well">
-							<h3> Online Users</h3>
-							<ul className="list-group" id="users"></ul>
+		 						<form id="messageForm">
+		 							<div className="form-group">
+										<label> Enter Message </label>
+		 								<textarea className="form-control" id="message" placeholder="Enter message..."></textarea>
+		 								<br />
+										<input type="submit" className="btn btn-primary" value="Send Message" />
+									</div>
+		 						</form>
 						</div>
-					</div>
-					<div className="col-md-8 offset-md-8">
-						<div className="chat" id="chat"></div>
+	 				</div>
+	)
+}
 
-						<form id="messageForm" onSubmit={this.handleSubmit}>
-							<div className="form-group">
-								<label> Enter Message </label>
-								<textarea className="form-control" id="message" value={this.state.value} onChange={this.handleChange}></textarea>
-								<br />
-								<input type="submit" className="btn btn-primary" value="Send Message" />
-							</div>
-						</form>
-					</div>
-				</div>
-        )
-    }
-};
+export default Room;
+
+// export default class Room extends Component {
+// 	constructor(props){
+// 		super(props);
+// 		this.state = {
+// 		  value: ""
+// 		};
+		
+// 		this.handleChange = this.handleChange.bind(this);
+// 		this.handleSubmit = this.handleSubmit.bind(this);
+// 	  }
+  
+// 	  //create methods
+// 	  handleChange(event){
+// 		this.setState({value: event.target.value});
+// 	  }
+  
+// 	  handleSubmit(event){
+// 		console.log('Message ' + this.state.value + ' Submitted')
+// 		//stop the form from refreshing the page on submit
+// 		event.preventDefault();
+// 		//this will clear input after submit
+// 		this.setState({ value: ""});
+// 	  }
+  
+//     render(){
+//         return (
+//             <div id="messageArea" className="row">
+// 					<div className="col-md-3">
+// 						<div className="well">
+// 							<h3> Online Users</h3>
+// 							<ul className="list-group" id="users"></ul>
+// 						</div>
+// 					</div>
+// 					<div className="col-md-8 offset-md-8">
+// 						<div className="chat" id="chat"></div>
+
+// 						<form id="messageForm" onSubmit={this.handleSubmit}>
+// 							<div className="form-group">
+// 								<label> Enter Message </label>
+// 								<textarea className="form-control" id="message" value={this.state.value} onChange={this.handleChange}></textarea>
+// 								<br />
+// 								<input type="submit" className="btn btn-primary" value="Send Message" />
+// 							</div>
+// 						</form>
+// 					</div>
+// 				</div>
+//         )
+//     }
+// };
 
