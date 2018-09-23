efa922391b944c890b81979579b68076e1ae2516
Finskou <lelarhaz@gmail.com>
2018-09-23 23:55:27 +0200

    commit to change branch

--- chat/src/components/Room.js
+++ chat/src/components/Room.js
@@ -7,6 +7,7 @@ export default class Room extends Component {
 		this.state = {
 		  value: ""
 		};
+
 		
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
