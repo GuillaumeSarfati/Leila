196119131ffed672aed8ecad9b898ff6ae43e75c
Finskou <lelarhaz@gmail.com>
2018-09-24 23:52:18 +0200

    cleaned up component and code

--- chat/src/App.js
+++ chat/src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import InputUsername  from './components/InputUsername';
 
+
 export default class App extends Component {
 
   render() {
     return (
       <div className="App">
 							<InputUsername />
-			</div>
+				</div>
     );
   }
 }
--- chat/src/components/InputUsername.js
+++ chat/src/components/InputUsername.js
@@ -1,37 +1,20 @@
 import React, { Component } from 'react'; 
 
 
+
 export default class InputUsername extends Component {
-    //initial state
-    constructor(props){
-      super(props);
-      this.state = {
-        value: ""
-      };
-      
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-
-    //create methods
-    handleChange(event){
-      this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event){
-      console.log('Username ' + this.state.value + ' Submitted')
-      event.preventDefault();
-      //this will clear input after submit
-      this.setState({value: ""});
-    }
-
     render() {
       return (
         <div className="inputusername">
           <form id="userform" onSubmit={this.handleSubmit} ref="form">
 									<div className="form-group">
 										<label> Enter Username </label>
-										<input type="text" className="form-control" id="username" value={this.state.value} onChange={this.handleChange}></input>
+                    <input 
+                      type="text" 
+                      className="form-control" 
+                      id="username" 
+                      >
+                    </input>
 										<br />
 										<input type="submit" className="btn btn-primary" value="Login"></input>
 									</div>
@@ -40,3 +23,4 @@ export default class InputUsername extends Component {
       );
     }
   }
+  
