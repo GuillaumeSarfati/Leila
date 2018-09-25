9c426a1a3c7de8bbcf83708fb4b6894702ddb916
Leila Rhazaoui <36553133+Finskou@users.noreply.github.com>
2018-09-25 14:40:37 +0200

    Update InputUsername.js

--- chat/src/components/InputUsername.js
+++ chat/src/components/InputUsername.js
@@ -7,18 +7,18 @@ export default class InputUsername extends Component {
       return (
         <div className="inputusername">
           <form id="userform" onSubmit={this.handleSubmit} ref="form">
-									<div className="form-group">
-										<label> Enter Username </label>
+		<div className="form-group">
+		<label> Enter Username </label>
                     <input 
                       type="text" 
                       className="form-control" 
                       id="username" 
                       >
                     </input>
-										<br />
-										<input type="submit" className="btn btn-primary" value="Login"></input>
-									</div>
-					</form>
+		    <br />
+		    <input type="submit" className="btn btn-primary" value="Login"></input>
+		</div>
+	    </form>
         </div>
       );
     }
